5a7d33010e0182979f7291531dbed091def30f12
milos-prince <mike.pinder@gmail.com>
2021-01-16 17:04:03 -0500

    homepage, our team and about done for desktop

--- src/components/homepageFeaturedLawyers.js
+++ src/components/homepageFeaturedLawyers.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const homepageLawyers = () => (
 <div className="homepage-lawyers">
     <h2 className="uppercase">Our team</h2>
-    <div className="d-flex">
+    <div className="d-flex homepage-team-text">
       <h3 className= "max-500">A team of legal professionals you can trust.</h3>
       <p className="max-500 ml-5vw">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in dolor scelerisque, porta elit ac, porta nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in dolor scelerisque, porta elit ac, porta nibh.</p>
     </div>
